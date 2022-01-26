76ba8228972bb0a87f428616c44cfe7dc293335a
vladk7072 <vladk7072@gmail.com>
2022-01-27 00:03:07 +0200

    27.01.2022

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,6 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 $(function () {
-
   $(".find__btn").click(function () {
     if (!$(this).hasClass("find__btn--active")) {
       $(".find__btn--active").removeClass("find__btn--active");
@@ -25,6 +24,15 @@ $(function () {
     $($(this)).addClass("chart__slide--active");
   });
 
+  $(".header__search-btn").click(function () {
+    $(".header__input").toggleClass("header__input--active");
+    $(".header__menu").removeClass("header__menu--active");
+  });
+  $(".header__burger-btn").click(function () {
+    $(".header__menu").toggleClass("header__menu--active");
+    $(".header__input").removeClass("header__input--active");
+  });
+
   const fslider = new Swiper(".chart__slider", {
     slidesPerView: 11,
     spaceBetween: 15,
@@ -33,5 +41,29 @@ $(function () {
       nextEl: ".chart__button-next",
       prevEl: ".chart__button-prev",
     },
+  });
+
+  var clientWidth = document.body.clientWidth;
+
+  function resizeTaleSlider(clientWidth) {
+    if (clientWidth < 611) {
+      $(".ford-choose__slider").addClass("swiper");
+      $(".ford-choose__items").addClass("swiper-wrapper");
+      $(".ford-choose__item-slider").addClass("swiper-slide");
+      var cslider = new Swiper(".ford-choose__slider",{
+        spaceBetween: 15,
+        pagination: {
+          el: '.ford-choose__slider-pagination',
+          clickable: true
+        },
+      });
+    }
+  }
+
+  resizeTaleSlider(clientWidth);
+
+  $(window).resize(function() {
+    let clientWidth = document.body.clientWidth;
+    resizeTaleSlider(clientWidth);
   });
 });
